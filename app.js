8d482c2f7df03735f3700462556036ef37e8930b
dinesh kumar <dk23818@gmail.com>
2025-04-06 17:55:30 +0530

    add: chapter 2

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "abc" },
   "This is react heading"
 );
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},[
-        React.createElement("div", {id:"child"},[
-            React.createElement("h1", {}, "I am a h1 tag"),
-            React.createElement("h2",{}, "I am a h2 tag")
-        ]),
-        React.createElement("div", {id:"child2"},[
-            React.createElement("h1",{}, "I am a h1 tag"),
-            React.createElement("h2", {}, "I am a h2 tag")
-        ])
-    ]
-)
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+]);
 
 console.log(heading); // object
 
