85f76352519115504341d39f58440eebb6a8ae56
dinesh kumar <dk23818@gmail.com>
2025-04-08 00:30:08 +0530

    chapter 3

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "This is react heading"
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading",},
+//   "Namaste React 🚀"
+// );
+
+// to create JSX element in multiple lines, we need to wrap it in ()
+// React Element
+const heading = (
+  <h1 className="heading" tabIndex="5">
+    Namaste React Using JSX
+  </h1>
 );
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-]);
+// React Functional Component
+const HeadingComponent = () => {
+  <h1>This is functional component</h1>;
+};
 
-console.log(heading); // object
+// if we have to write it in one line, we can write it like this too, without {} and return keyword
+const HeadingComponent2 = () => <h1>React Functional Component</h1>;
+
+//it can also be written like this without return keyword
+const HeadingComponent3 = () => (
+    <h1>React Functional Component</h1>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(parent);
+root.render(heading);
